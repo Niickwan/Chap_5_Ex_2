5a76d368e8440e763660c21a0fc937609ed2f93c
Niickwan <89967473+Niickwan@users.noreply.github.com>
2021-09-11 15:20:03 +0200

    Added new method to avoid repeating code, removed unused int.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,36 +5,32 @@ import java.util.Random;
 public class Main {
 
     public static boolean ShouldWeGoAgain (int x) {
-        Scanner scanner = new Scanner(System.in);
         if (x == 2) {
             System.out.println("Too many tries, do you want to try again? (y/n)");
-            String startAgain = scanner.next();
-            if (startAgain .equalsIgnoreCase("y")) {
-                System.out.println("Here we go gain!\n\n");
-                return true;
-            } else {
-                System.out.println("Thanks for playing");
-                return false;
-            }
+            return ShouldWeGoAgainInput();
         } else if (x > 2) {
             System.out.println("Do you want to go again? (y/n)");
-            String startAgain = scanner.next();
-            if (startAgain .equalsIgnoreCase("y")) {
-                System.out.println("Here we go gain!\n\n");
-                return true;
-            } else {
-                System.out.println("Thanks for playing");
-                return false;
-            }
+            return ShouldWeGoAgainInput();
         }
         return false;
+    }
+
+    public static boolean ShouldWeGoAgainInput () {
+        Scanner scanner = new Scanner(System.in);
+        String startAgain = scanner.next();
+        if (startAgain .equalsIgnoreCase("y")) {
+            System.out.println("Here we go gain!\n");
+            return true;
+        } else {
+            System.out.println("Thanks for playing!");
+            return false;
+        }
     }
 
     public static void main(String[] args) {
         // Pick a random number
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int difference;
         int counter = 0;
         int number = 0;
         boolean startOver = true;
@@ -43,7 +39,7 @@ public class Main {
         while (!exit) {
             if (startOver){
                 number = random.nextInt(100) + 1;
-                System.out.println("I'm thinking of a number between 1 and 100 (including both). you have 3 tries, can you guess what it is?");
+                System.out.println("I'm thinking of a number between 1 and 100 (including both). you have 3 guesses, can you guess what it is?");
                 startOver = false;
             }
             if (counter < 3) {
@@ -52,11 +48,9 @@ public class Main {
                     int guess = scanner.nextInt();
                     System.out.println("Your guess is: " + guess);
                     if (number > guess) {
-                        difference = number - guess;
                         System.out.println("Your guess was too low");
                         counter++;
                     } else if (number < guess) {
-                        difference = guess - number;
                         System.out.println("Your guess was too high");
                         counter++;
                     } else if (number == guess) {
